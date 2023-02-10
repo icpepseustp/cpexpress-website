814350c99df5f954e524916fa3ada61b4b6bef0f
diffident016 <marvin.tagolimotjr13@gmail.com>
2023-02-10 17:41:13 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center h-screen items-center'>
+     <h1 className='text-offwhite text-3xl font-bold'>COMWEB Cutie</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: "#06162E",
+        secondary: "##0E2C4E",
+        tertiary: "#334B66",
+        offwhite: "#5E85B0",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
